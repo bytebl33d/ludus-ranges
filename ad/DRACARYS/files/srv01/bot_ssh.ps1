da1814f92bd84afb32d33772d40c329fb1ee98b6
bytebl33d <61542339+bytebl33d@users.noreply.github.com>
2026-08-09 06:39:18 -0700

    Fix DNS issues and DRACARYS bot

--- ad/DRACARYS/files/srv01/bot_ssh.ps1
+++ ad/DRACARYS/files/srv01/bot_ssh.ps1
@@ -1,5 +1,5 @@
 $User = "viserion"
-$SSHHost = "syrax"
+$SSHHost = "syrax.dracarys.lab"
 $Password = "aLHtz1WvIVmeV4Zh4CDE"
 
 & "C:\Program Files\PuTTY\klink.exe" -auto_store_sshkey $SSHHost -l "$User" -pw $Password "sleep 45"
